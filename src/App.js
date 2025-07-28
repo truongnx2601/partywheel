d24d70086fae772ccae7118a7f8d4c8b54e6583e
Nguyen Truong <truongnguyen26011994@gmail.com>
2025-07-28 14:06:02 +0700

    update name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Wheel from "./Wheel";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-yellow-100 to-pink-200">
-      <h1 className="text-3xl font-bold text-red-600 mb-8">🎉 Vòng Quay Mai Off</h1>
+      <h1 className="text-3xl font-bold text-red-600 mb-8">🎉 Vòng Quay "Hên lắm mới xui được vậy"</h1>
       <Wheel />
     </div>
   );
